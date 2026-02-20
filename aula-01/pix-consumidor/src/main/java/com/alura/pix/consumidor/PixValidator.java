1d2cf43ec18ea9c562324121619ad9ab8fda281b
Henrytos <henry.arcaya@sptech.school>
2026-02-20 11:36:03 -0300

    configurando retentativas de mensagens

--- aula-01/pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
+++ aula-01/pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
@@ -2,13 +2,16 @@ package com.alura.pix.consumidor;
 
 import com.alura.pix.dto.PixDTO;
 import com.alura.pix.dto.PixStatus;
+import com.alura.pix.exception.KeyNotFoundException;
 import com.alura.pix.model.Key;
 import com.alura.pix.model.Pix;
 import com.alura.pix.repository.KeyRepository;
 import com.alura.pix.repository.PixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +24,14 @@ public class PixValidator {
     private PixRepository pixRepository;
 
     @KafkaListener(topics = "pix-topic", groupId = "grupo")
+    @RetryableTopic(
+            attempts = "5",
+            backoff = @Backoff(
+                    value = 5000L
+            ),
+            autoCreateTopics = "true",
+            include = Exception.class
+    )
     public void processaPix(PixDTO pixDTO, Acknowledgment acknowledgment) {
 
         System.out.println("Recebendo pix: " + pixDTO.getIdentifier());
@@ -35,6 +46,7 @@ public class PixValidator {
                 || !pix.getChaveDestino().equals(pix.getChaveDestino())
         ) {
             pix.setStatus(PixStatus.ERRO);
+            throw new KeyNotFoundException();
         } else {
             pix.setStatus(PixStatus.PROCESSADO);
         }
